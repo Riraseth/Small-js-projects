b29a50ca64df2f8e0587a6c89133d76eb2e7089e
Łukasz Zabuski <lukasz@biuro.bcweb.pl>
2020-02-09 23:02:26 +0100

    close all change

--- 10_accordion/main.js
+++ 10_accordion/main.js
@@ -1,15 +1,18 @@
 const accordion = document.querySelectorAll('.panel');
-// accordion.forEach(item => {
-//   item.addEventListener('click', e => {
-//     accordion.forEach(item => {
-//       item.classList.remove('active')
-//     })
-//     e.currentTarget.classList.add('active')
-//   })
-// })
-
 accordion.forEach(item => {
   item.addEventListener('click', e => {
-    e.currentTarget.classList.toggle('active')
+    accordion.forEach(ele => {
+      if (e.currentTarget === ele) {
+        ele.classList.toggle('active')
+      } else {
+        ele.classList.remove('active')
+      }
+    })
   })
 })
+
+// accordion.forEach(item => {
+//   item.addEventListener('click', e => {
+//     e.currentTarget.classList.toggle('active')
+//   })
+// })
