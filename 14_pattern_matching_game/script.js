00ad323933b568db5995f49ac400d1ef5aef9a7d
Łukasz Zabuski <lukasz@biuro.bcweb.pl>
2020-02-15 22:28:22 +0100

    initial commit

---
+++ 14_pattern_matching_game/script.js
@@ -0,0 +1,7 @@
+const messager = document.querySelector('.message');
+const gamearea = document.querySelector('.gamearea');
+const btn = document.querySelector('button');
+
+btn.addEventListener('click', () => {
+  console.log('works')
+})
