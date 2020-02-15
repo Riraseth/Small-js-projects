4d09b5592516b2eb6a1d116dd1f25b736f1a9f0d
Łukasz Zabuski <lukasz@biuro.bcweb.pl>
2020-02-15 21:49:34 +0100

    countdown working

--- 13_countdown_timer/script.js
+++ 13_countdown_timer/script.js
@@ -1,20 +1,31 @@
 const endDate = document.querySelector('input');
 const clock = document.querySelector('clock');
+let timeInterval;
+let timeStop = false;
 
 endDate.addEventListener('change', e => {
   e.preventDefault();
+  clearInterval(timeInterval);
   const temp = new Date(endDate.value);
-  console.log(temp);
   startClock(temp);
+  timeStop = false;
 });
 
 const startClock = date => {
-  timeLeft(date);
+  const updateCounter = () => {
+    displayTimeLeft(date)
+  }
+  updateCounter();
+  if (!timeStop) {
+    timeInterval = setInterval(updateCounter, 1000);
+  } else {
+    clearInterval(timeInterval);
+  }
 }
 
 const timeLeft = date => {
   let currentDate = new Date();
-  let time = Date.parse(date) - date.parse(currentDate);
+  let time = Date.parse(date) - Date.parse(currentDate);
   let seconds = Math.floor((time / 1000) % 60);
   let minutes = Math.floor((time / 1000 / 60) % 60);
   let hours = Math.floor((time / (1000 * 60 * 60)) % 24);
@@ -26,4 +37,19 @@ const timeLeft = date => {
     'minutes': minutes,
     'seconds': seconds
   }
+}
+
+const displayTimeLeft = date => {
+  let tl = timeLeft(date);
+  if (tl.total <= 0) {
+    timeStop = true;
+    console.log(timeStop)
+  }
+  let entries = Object.entries(tl);
+  entries.forEach(entry => {
+    let el = document.querySelector(`.${entry[0]}`);
+    if (el) {
+      el.textContent = entry[1];
+    }
+  })
 }
