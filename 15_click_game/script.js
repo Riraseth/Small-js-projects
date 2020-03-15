32b22f0c884e3a4da7b22d3507ebfbb032720917
Łukasz <riraseth@gmail.com>
2020-03-15 23:22:11 +0100

    v1

--- 15_click_game/script.js
+++ 15_click_game/script.js
@@ -6,27 +6,57 @@ playArea.main = document.querySelector('.main');
 playArea.game = document.querySelector('.game');
 playArea.btns = document.querySelectorAll('.btn');
 playArea.page = document.querySelectorAll('.page');
-document.addEventListener('DOMContentLoaded', getData);
+document.addEventListener('DOMContentLoaded', getData());
+
+async function getData() {
+  playArea.main.classList.add('visible');
+  try {
+    const response = await fetch('https://api.myjson.com/bins/htsg4');
+    const result = await response.json();
+    gameObj = await result.data;
+    buildBoard();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function buildBoard() {
+  playArea.scorer = document.createElement('span');
+  playArea.scorer.innerHTML = 'Press button to start';
+  playArea.stats.appendChild(playArea.scorer);
+  let rows = 4;
+  let cols = 4;
+  let count = 0;
+  playArea.game.style.width = cols * 100 + cols * 2;
+  playArea.game.style.margin = '40 auto 0 auto';
+  for (let i = 0; i < rows; i++) {
+    let divMain = document.createElement('div');
+    divMain.setAttribute('class', 'row');
+    divMain.style.width = cols * 100 + cols * 2;
+    for (let j = 0; j < cols; j++) {
+      let div = document.createElement('div');
+      div.setAttribute('class', 'pop');
+      div.style.cursor = 'pointer';
+      count++;
+      div.textContent = count;
+      div.count = count;
+      divMain.appendChild(div);
+    }
+    playArea.game.appendChild(divMain);
+  }
+}
+
+function updateScore() {
+  playArea.scorer.innerHTML = `Score: ${player.score} Lives: ${player.lives}`;
+}
 
 let gameObj;
 playArea.btns.forEach(btn => {
-  btn.addEventListener('click', handleBtn)
-})
-
-function getData() {
-  playArea.main.classList.add('visible')
-  fetch("https://api.myjson.com/bins/htsg4").then(response => {
-    return response.json();
-  }).then(myJson => {
-    gameObj = myJson.data;
-    buildBoard();
-  }).catch((error) => {
-    console.error('There has been a problem with your fetch operation:', error)
-  })
-}
+  btn.addEventListener('click', handleBtn);
+});
 
 function handleBtn(e) {
-  if (e.target.classList.contains('new-game')) {
+  if (e.target.classList.contains('newGame')) {
     startGame();
   }
 }
@@ -38,42 +68,61 @@ function startGame() {
   playArea.game.classList.add('visible');
   player.gameOver = false;
   startPop();
+  updateScore();
 }
 
 function startPop() {
-  let newPop = random();
+  let newPop = randomPop();
   newPop.classList.add('active');
+  newPop.addEventListener('click', hitPop);
+  const time = Math.round(Math.random() * 1500 + 750);
+  const val = Math.floor(Math.random() * gameObj.length);
+  newPop.old = newPop.textContent;
+  newPop.v = gameObj[val].value;
+  newPop.innerHTML = gameObj[val].icon + '<br>' + gameObj[val].value;
+  playArea.inPlay = setTimeout(() => {
+    newPop.classList.remove('active');
+    newPop.removeEventListener('click', hitPop);
+    newPop.textContent = newPop.old;
+    if (newPop.v > 0) {
+      player.lives--;
+      updateScore();
+    }
+    if (player.lives <= 0) {
+      gameOver();
+    }
+    if (!player.gameOver) {
+      startPop();
+    }
+  }, time);
 }
 
-function random() {
+function gameOver() {
+  player.gameOver = true;
+  playArea.main.classList.add('visible');
+  playArea.game.classList.remove('visible');
+  document.querySelector('.newGame').textContent = 'Try again';
+}
+
+function randomPop() {
   const pops = document.querySelectorAll('.pop');
   const idx = Math.floor(Math.random() * pops.length);
   if (pops[idx].count == playArea.last) {
-    return random();
+    return randomPop();
   }
   playArea.last = pops[idx].count;
   return pops[idx];
 }
 
-function buildBoard() {
-  let rows = 4;
-  let cols = 4;
-  let count = 0;
-  playArea.game.style.width = cols * 100 + (cols * 2);
-  playArea.game.style.margin = '40 auto 0 auto';
-  for (let i = 0; i < rows; i++) {
-    let divMain = document.createElement('div');
-    divMain.setAttribute('class', 'row');
-    divMain.style.width = cols * 100 + (cols * 2);
-    for (let j = 0; j < cols; j++) {
-      let div = document.createElement('div');
-      div.setAttribute('class', 'pop');
-      div.style.cursor = 'pointer';
-      count++;
-      div.textContent = count;
-      div.count = count;
-      divMain.appendChild(div);
-    }
-    playArea.game.appendChild(divMain)
+function hitPop(e) {
+  let newPop = e.target;
+  player.score = player.score + newPop.v;
+  updateScore();
+  newPop.classList.remove('active');
+  newPop.removeEventListener('click', hitPop);
+  newPop.textContent = newPop.old;
+  clearTimeout(playArea.inPlay);
+  if (!player.gameOver) {
+    startPop();
   }
 }
